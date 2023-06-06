9ba073c00591fe548288771fe461495426b601a6
karthik-m96 <karthikm300796@gmail.com>
2023-06-06 18:48:04 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Form from './Components/Form/Form'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./form.scss";
+
+const Form = () => {
+  return (
+    <div className="form">
+      <div className="title">
+        <h2>Customer Information Master</h2>
+      </div>
+      <div className="container">
+        <div className="nameEmail">
+          <div className="name">
+            <label htmlFor="name">Customer's Name:</label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter the name"
+              required
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="email">Customer's email address:</label>
+            <br />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter the email address"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="phone">
+          <label htmlFor="phone">Customer's phone number:</label>
+          <br />
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter the phone number"
+            required
+          />
+        </div>
+        <div className="dropDown">
+          <label htmlFor="typeofenq">Select the type of enquiry: </label>
+          <br />
+          <select className="select" name="typeofenq" id="enq">
+            <option value="sales">Sales</option>
+            <option value="Orders">Orders</option>
+            <option value="Payment">Payment</option>
+          </select>
+        </div>
+        <div className="field">
+          <label htmlFor="explanation">Explain the enquiry in detail:</label>
+          <br />
+          <textarea className="textArea" cols="50" rows="10"></textarea>
+        </div>
+        <div className="submit">
+          <input type="submit" value="submit" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
